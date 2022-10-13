26b666e38e488088de30e098407dc28bd16c6391
INLL <waileungsunny@gmail.com>
2022-10-14 02:06:43 +0800

    Completed some more challenges

--- test.js
+++ test.js
@@ -1,8 +1,53 @@
 import { useState } from 'react';
+import { foods, filterItems } from './data.js';
 
-export default function FeedbackForm() {
-  const initialItems = [
-    { title: 'pretzels', id: 0 },
-    { title: 'crispy seaweed', id: 1 },
-    { title: 'granola bar', id: 2 },
-  ];
+export default function FilterableList() {
+  const [query, setQuery] = useState('');
+
+  function handleInput(e){
+    setQuery(e.target.value);
+  }
+
+  return (
+    <>
+      <SearchBar 
+        onChange={handleInput}
+        value={query}
+      />
+      <hr />
+      <List 
+        items={foods} 
+        query={query}
+      />
+    </>
+  );
+}
+
+function SearchBar({ onChange, value }) {
+  return (
+    <label>
+      Search:{' '}
+      <input
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+}
+
+function List({ items, query }) {
+  return (
+    <table>
+      <tbody>
+        {
+          filterItems(items, query).map(item => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.description}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  );
+}
