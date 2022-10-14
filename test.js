474ae3e15231b4a526af36781ba6de2bcc7fa279
INLL <waileungsunny@gmail.com>
2022-10-14 21:30:05 +0800

    Some more challenges completed

--- test.js
+++ test.js
@@ -1,53 +1,109 @@
-import { useState } from 'react';
-import { foods, filterItems } from './data.js';
+import {useState} from 'react';
 
-export default function FilterableList() {
-  const [query, setQuery] = useState('');
-
-  function handleInput(e){
-    setQuery(e.target.value);
-  }
-
+export default function Chat({contact, message, dispatch}) {
   return (
-    <>
-      <SearchBar 
-        onChange={handleInput}
-        value={query}
+    <section className="chat">
+      <textarea
+        value={message}
+        placeholder={'Chat to ' + contact.name}
+        onChange={(e) => {
+          dispatch({
+            type: 'edited_message',
+            message: e.target.value,
+          })
+        }}
       />
-      <hr />
-      <List 
-        items={foods} 
-        query={query}
-      />
-    </>
+      <br />
+      <button>Send to {contact.email}</button>
+    </section>
   );
 }
 
-function SearchBar({ onChange, value }) {
+
+selectedId: action.contactId,
+message: '',
+
+dispatch({
+  type: 'changed_selection',
+  selectedId: contact.id,
+  message: '',
+})
+
+
+import {useReducer} from 'react';
+import Chat from './Chat.js';
+import ContactList from './ContactList.js';
+import {initialState, messengerReducer} from './messengerReducer';
+
+export default function Messenger() {
+  const [state, dispatch] = useReducer(messengerReducer, initialState);
+  const message = state.message;
+  const contact = contacts.find((c) => c.id === state.selectedId);
   return (
-    <label>
-      Search:{' '}
-      <input
-        value={value}
-        onChange={onChange}
+    <div>
+      <ContactList
+        contacts={contacts}
+        selectedId={state.selectedId}
+        dispatch={dispatch}
       />
-    </label>
+      <Chat
+        key={contact.id}
+        message={message}
+        contact={contact}
+        dispatch={dispatch}
+      />
+    </div>
   );
 }
 
-function List({ items, query }) {
+const contacts = [
+  {id: 0, name: 'Taylor', email: 'taylor@mail.com'},
+  {id: 1, name: 'Alice', email: 'alice@mail.com'},
+  {id: 2, name: 'Bob', email: 'bob@mail.com'},
+];
+
+
+export default function ContactList({contacts, selectedId, dispatch}) {
   return (
-    <table>
-      <tbody>
-        {
-          filterItems(items, query).map(item => (
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              <td>{item.description}</td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
+    <section className="contact-list">
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            <button
+              onClick={() => {
+                dispatch({
+                  type: 'changed_selection',
+                  contactId: contact.id,
+                });
+              }}>
+              {selectedId === contact.id ? <b>{contact.name}</b> : contact.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
+
+
+import {useState} from 'react';
+
+export default function Chat({contact, message, dispatch}) {
+  return (
+    <section className="chat">
+      <textarea
+        value={message}
+        placeholder={'Chat to ' + contact.name}
+        onChange={(e) => {
+          dispatch({
+            type: 'edited_message',
+            message: e.target.value,
+          });
+        }}
+      />
+      <br />
+      <button>Send to {contact.email}</button>
+    </section>
+  );
+}
+
--- test2.js
+++ test2.js
@@ -1,30 +1,32 @@
-export function filterItems(items, query) {
-  query = query.toLowerCase();
-  return items.filter(item =>
-    item.name.split(' ').some(word =>
-      word.toLowerCase().startsWith(query)
-    )
-  );
-}
+import { useReducer } from 'react';
 
-export const foods = [{
-  id: 0,
-  name: 'Sushi',
-  description: 'Sushi is a traditional Japanese dish of prepared vinegared rice'
-}, {
-  id: 1,
-  name: 'Dal',
-  description: 'The most common way of preparing dal is in the form of a soup to which onions, tomatoes and various spices may be added'
-}, {
-  id: 2,
-  name: 'Pierogi',
-  description: 'Pierogi are filled dumplings made by wrapping unleavened dough around a savoury or sweet filling and cooking in boiling water'
-}, {
-  id: 3,
-  name: 'Shish kebab',
-  description: 'Shish kebab is a popular meal of skewered and grilled cubes of meat.'
-}, {
-  id: 4,
-  name: 'Dim sum',
-  description: 'Dim sum is a large range of small dishes that Cantonese people traditionally enjoy in restaurants for breakfast and lunch'
-}];
+function Counter() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  function reducer(count, action) {
+    switch(action.type) {
+      case 'increment': {
+        return count + 1;
+      }
+      default:
+        return count
+    }
+  }
+
+  function changeCount(amount) {
+    setCount(prevCount => prevCount + amount);
+  }
+
+  function resetCount() {
+    setCount(0);
+  }
+
+  return (
+    <>
+      <span>{count}</span>
+      <button onClick={() => changeCount(1)}>+</button>
+      <button onClick={() => changeCount(-1)}>-</button>
+      <button onClick={() => resetCount()}>Reset</button>
+    </>
+  )
+}
