fe9be138c31e9e1b3c376acd31ced0260fef7a82
INLL <waileungsunny@gmail.com>
2022-10-16 19:57:45 +0800

    some solutions

--- test2.js
+++ test2.js
@@ -1,32 +1,62 @@
-import { useReducer } from 'react';
+import { useState } from 'react';
+import { places } from './data.js';
+import { getImageUrl } from './utils.js';
 
-function Counter() {
-  const [count, dispatch] = useReducer(reducer, 0);
-
-  function reducer(count, action) {
-    switch(action.type) {
-      case 'increment': {
-        return count + 1;
-      }
-      default:
-        return count
-    }
-  }
-
-  function changeCount(amount) {
-    setCount(prevCount => prevCount + amount);
-  }
-
-  function resetCount() {
-    setCount(0);
-  }
-
+export default function App() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
   return (
     <>
-      <span>{count}</span>
-      <button onClick={() => changeCount(1)}>+</button>
-      <button onClick={() => changeCount(-1)}>-</button>
-      <button onClick={() => resetCount()}>Reset</button>
+      <label>
+        <input
+          type="checkbox"
+          checked={isLarge}
+          onChange={e => {
+            setIsLarge(e.target.checked);
+          }}
+        />
+        Use large images
+      </label>
+      <hr />
+      <List imageSize={imageSize} />
     </>
   )
+}
+
+function List({ imageSize }) {
+  const listItems = places.map(place =>
+    <li key={place.id}>
+      <Place
+        place={place}
+        imageSize={imageSize}
+      />
+    </li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function Place({ place, imageSize }) {
+  return (
+    <>
+      <PlaceImage
+        place={place}
+        imageSize={imageSize}
+      />
+      <p>
+        <b>{place.name}</b>
+        {': ' + place.description}
+      </p>
+    </>
+  );
+}
+
+function PlaceImage({ place, imageSize }) {
+  return (
+    <img
+      src={getImageUrl(place)}
+      alt={place.name}
+      width={imageSize}
+      height={imageSize}
+    />
+  );
 }
